7ec2f38a76ff470a1cca0f383091a9ada645a8bd
Unleashed1 <39225934+Unleashed1@users.noreply.github.com>
2022-05-22 16:48:03 +0200

    Add files via upload

--- server.c
+++ server.c
@@ -9,11 +9,39 @@
 //the thread function
 void *connection_handler(void *);
 
+void* get_ip(){
+	int n;
+
+struct ifreq ifr;
+
+char array[] = "eth0";
+
+
+
+n = socket(AF_INET, SOCK_DGRAM, 0);
+
+//Type of address to retrieve - IPv4 IP address
+
+ifr.ifr_addr.sa_family = AF_INET;
+
+//Copy the interface name in the ifreq structure
+
+strncpy(ifr.ifr_name , array , IFNAMSIZ - 1);
+
+ioctl(n, SIOCGIFADDR, &ifr);
+
+close(n);
+
+//display result
+
+printf("IP Address is %s - %s\n" , array , inet_ntoa(( (struct sockaddr_in *)&ifr.ifr_addr )->sin_addr) );
+}
+
 int main(int argc , char *argv[])
 {
 	int socket_desc , client_sock , c , *new_sock;
 	struct sockaddr_in server , client;
-	
+	get_ip();
 	//Create socket
 	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
 	if (socket_desc == -1)
@@ -21,12 +49,12 @@ int main(int argc , char *argv[])
 		printf("Could not create socket");
 	}
 	puts("Socket created");
-	
+
 	//Prepare the sockaddr_in structure
 	server.sin_family = AF_INET;
 	server.sin_addr.s_addr = INADDR_ANY;
 	server.sin_port = htons( 8989 );
-	
+
 	//Bind
 	if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
 	{
@@ -35,43 +63,43 @@ int main(int argc , char *argv[])
 		return 1;
 	}
 	puts("bind done");
-	
+
 	//Listen
 	listen(socket_desc , 3);
-	
+
 	//Accept and incoming connection
 	puts("Waiting for incoming connections...");
 	c = sizeof(struct sockaddr_in);
-	
-	
+
+
 	//Accept and incoming connection
 	puts("Waiting for incoming connections...");
 	c = sizeof(struct sockaddr_in);
 	while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
 	{
 		puts("Connection accepted");
-		
+
 		pthread_t sniffer_thread;
 		new_sock = malloc(1);
 		*new_sock = client_sock;
-		
+
 		if( pthread_create( &sniffer_thread , NULL ,  connection_handler , (void*) new_sock) < 0)
 		{
 			perror("could not create thread");
 			return 1;
 		}
-		
+
 		//Now join the thread , so that we dont terminate before the thread
 		//pthread_join( sniffer_thread , NULL);
 		puts("Handler assigned");
 	}
-	
+
 	if (client_sock < 0)
 	{
 		perror("accept failed");
 		return 1;
 	}
-	
+
 	return 0;
 }
 
@@ -84,7 +112,7 @@ void *connection_handler(void *socket_desc)
 	int sock = *(int*)socket_desc;
 	int read_size;
 	char *message , client_message[2000];
-	
+
 	//Send some messages to the client
 	message = "Are you worthy of wielding Mjolnir?\n";
 	write(sock , message , strlen(message));
@@ -93,14 +121,14 @@ void *connection_handler(void *socket_desc)
 	{
 		if(client_message=="Mjolnir"){
 			message ="Now you ave the root power of Mjolnir\n";
-			write(sock, message, strlen(message)); 
+			write(sock, message, strlen(message));
 		}else{
 			message ="No you are not!\n";
 			write(sock,message,strlen(message));
-			
+
 		}
 	}
-	
+
 	if(read_size == 0)
 	{
 		puts("Client disconnected");
@@ -110,9 +138,9 @@ void *connection_handler(void *socket_desc)
 	{
 		perror("recv failed");
 	}
-		
+
 	//Free the socket pointer
 	free(socket_desc);
-	
+
 	return 0;
 }
